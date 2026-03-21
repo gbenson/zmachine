ccac380b456009dbcb3a11ebbd8c67f755a79778
Gary Benson <gary@gbenson.net>
2026-03-21 14:23:41 +0000

    util.Logger, Allow explicitly setting component names

--- util/util.go
+++ util/util.go
@@ -6,6 +6,9 @@ import (
 )
 
 func ComponentName(comp any) string {
+	if name, ok := comp.(string); ok {
+		return name
+	}
 	name := fmt.Sprintf("%T", comp)    // *package.Type
 	name = strings.TrimLeft(name, "*") // package.Type
 	return name
