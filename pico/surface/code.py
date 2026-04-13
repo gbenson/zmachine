d7c22a363c8b5219ffa7878533ef377b740917ea
Gary Benson <gary@gbenson.net>
2026-04-13 23:04:14 +0100

    surface, Emit volume messages too

--- pico/surface/code.py
+++ pico/surface/code.py
@@ -3,6 +3,8 @@ import time
 
 from adafruit_midi import MIDI
 from adafruit_midi.control_change import ControlChange
+from adafruit_midi.control_change_values import VOLUME
+from analogio import AnalogIn
 from digitalio import DigitalInOut, Direction, Pull
 from rotaryio import IncrementalEncoder
 from usb_midi import PortIn, PortOut, ports
@@ -63,13 +65,42 @@ class Encoder:
         return anychange
 
 
+class Potentiometer:
+    def __init__(self, pin, cc_msb, cc_lsb=-1, *, squelch=64):
+        if cc_lsb < 0:
+            cc_lsb = cc_msb + 32
+        self.cc_msb = cc_msb
+        self.cc_lsb = cc_lsb
+        self.pot = AnalogIn(pin)
+        self.lastval = self.pot.value
+        self.squelch = squelch
+
+    def update(self):
+        # Pico has 12-bit ADC, so 4096 possible values, but the
+        # library scales values up to uint16 (so 0..65535), then
+        # we then scale them down to the 14 bits we can fit in a
+        # regular (paired) MIDI CC.
+        val = self.pot.value >> 2
+        if abs(val - self.lastval) < self.squelch:
+            return False
+        print(f"potentiometer[{self.cc_msb}].value = {val}")
+
+        send_midi(ControlChange(self.cc_msb, (val >> 7) & 127))
+        if (cc_lsb := self.cc_lsb) is not None:
+            send_midi(ControlChange(cc_lsb, val & 127))
+
+        self.lastval = val
+        return True
+
+
 def main():
+    pots = [Potentiometer(board.A2, VOLUME)]  # ADC2 == GP28 == physical pin 34
     encoders = [
         Encoder(n, *pins) for n, pins in enumerate((
             (5, 6, 7),    # => 0  Locations:
             (8, 9, 10),   # => A
             (11, 12, 13), # => B    +-----------+
-            (14, 15, 28), # => C    | 0         |
+            (14, 15, 0),  # => C    | 0         |
             (26, 27, 22), # => D    |  ABCD     |
             (2, 3, 4),    # => X    | X Y Z     |
             (19, 20, 21), # => Y    +-----------+
@@ -79,6 +110,9 @@ def main():
 
     while True:
         anychange = False
+        for p in pots:
+            if p.update():
+                anychange = True
         for e in encoders:
             if e.update():
                 anychange = True
