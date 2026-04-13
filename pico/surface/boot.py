c2b88f036e18ab9b392f42760a7ba952ae851a3c
Gary Benson <gary@gbenson.net>
2026-04-13 23:56:24 +0100

    surface, Disable USB drive

--- pico/surface/boot.py
+++ pico/surface/boot.py
@@ -1,12 +1,27 @@
+import board
+import storage
 import supervisor
 import usb_hid
 import usb_midi
+
+from digitalio import DigitalInOut, Pull
 
 # Define our USB Device Descriptor vendor and product ID.
 supervisor.set_usb_identification(
     manufacturer="Zmachine",    # default: "Raspberry Pi"
     product="Control Surface",  # default: "Pico"
 )
+
+# Don't present as a USB drive unless the encoder whose
+# switch is on GP18 is pressed (i.e. GP18 is shorted to
+# GND).  ALSA doesn't see the I2S DAC for some reason if
+# the CIRCUITPY drive is present when the Pi boots (and
+# it's then *minutes* before anything shows on the OLED
+# or SSH comes up...)
+switch = DigitalInOut(board.GP18)
+switch.pull = Pull.UP
+if switch.value:
+    storage.disable_usb_drive()
 
 # Don't present as a USB keyboard and mouse to the host.
 # We show up as "Zmachine Control Surface Keyboard" and
