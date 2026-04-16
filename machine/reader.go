a6f021507a9e90049ed1e9668255db9e248f3de9
Gary Benson <gary@gbenson.net>
2026-04-16 19:53:15 +0100

    Remove an overkill log message

--- machine/reader.go
+++ machine/reader.go
@@ -4,18 +4,15 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"sync"
 	"unsafe"
 
 	. "gbenson.net/go/zmachine/core"
-	"gbenson.net/go/zmachine/util"
 )
 
 type reader struct {
 	ctx     context.Context
 	source  AudioSource
 	metrics readerMetrics
-	onRead  func()
 }
 
 // NewReader returns a new [io.Reader] reading from src.
@@ -25,20 +22,12 @@ func NewReader(ctx context.Context, src AudioSource) io.ReadCloser {
 	} else if src == nil {
 		panic("nil source")
 	}
-
-	return &reader{
-		ctx:    ctx,
-		source: src,
-		onRead: sync.OnceFunc(func() {
-			util.Logger(ctx, "zmachine").Info().Msg("Running")
-		}),
-	}
+	return &reader{ctx: ctx, source: src}
 }
 
 // Read implements [io.Reader].
 func (r *reader) Read(p []byte) (n int, err error) {
 	r.metrics.setWorking()
-	r.onRead()
 	defer r.metrics.setIdle()
 
 	sizeBytes := len(p)
