9c061daf92218dd3cab27d2ed654a4d8cdc8deb0
Gary Benson <gary@gbenson.net>
2026-04-29 21:33:04 +0100

    Move modules.{FunctionShaper,SampleShaper} into core

---
+++ core/adapters.go
@@ -0,0 +1,31 @@
+package core
+
+// FractionShaper is an adapter that allows the use of ordinary
+// functions as shapers.  If f is a function with the appropriate
+// signature, then FractionShaper(f) is a [Shaper] that calls f.
+type FractionShaper func(Fraction) Fraction
+
+// Fraction implements [Shaper].
+func (f FractionShaper) Fraction(x Fraction) Fraction {
+	return f(x)
+}
+
+// Sample implements [Shaper].
+func (f FractionShaper) Sample(x Fraction) Sample {
+	return Sample(f(x)*2 - 1)
+}
+
+// SampleShaper is an adapter that allows the use of ordinary
+// functions as shapers.  If f is a function with the appropriate
+// signature, then SampleShaper(f) is a [Shaper] that calls f.
+type SampleShaper func(Fraction) Sample
+
+// Fraction implements [Shaper].
+func (f SampleShaper) Fraction(x Fraction) Fraction {
+	return Fraction((f(x) + 1) / 2)
+}
+
+// Sample implements [Shaper].
+func (f SampleShaper) Sample(x Fraction) Sample {
+	return f(x)
+}
--- modules/function-shapers.go
+++ modules/function-shapers.go
@@ -6,46 +6,6 @@ import (
 	. "gbenson.net/go/zmachine/core"
 )
 
-// FractionShaper returns a [Shaper] that calls the given function.
-func FractionShaper(f func(Fraction) Fraction) Shaper {
-	return fractionShaper(f)
-}
-
-// fractionShaper is an adaptor that allows the use of ordinary
-// functions as [Shaper]s.  If f is a function with the appropriate
-// signature, then fractionShaper(f) is a [Shaper] that calls f.
-type fractionShaper func(Fraction) Fraction
-
-// Fraction implements [Shaper].
-func (f fractionShaper) Fraction(x Fraction) Fraction {
-	return f(x)
-}
-
-// Sample implements [Shaper].
-func (f fractionShaper) Sample(x Fraction) Sample {
-	return Sample(f(x)*2 - 1)
-}
-
-// SampleShaper returns a [Shaper] that calls the given function.
-func SampleShaper(f func(Fraction) Sample) Shaper {
-	return sampleShaper(f)
-}
-
-// sampleShaper is an adaptor that allows the use of ordinary
-// functions as [Shaper]s.  If f is a function with the appropriate
-// signature, then sampleShaper(f) is a [Shaper] that calls f.
-type sampleShaper func(Fraction) Sample
-
-// Fraction implements [Shaper].
-func (f sampleShaper) Fraction(x Fraction) Fraction {
-	return Fraction((f(x) + 1) / 2)
-}
-
-// Sample implements [Shaper].
-func (f sampleShaper) Sample(x Fraction) Sample {
-	return f(x)
-}
-
 // SineShaper generates a sine wave.
 var SineShaper = SampleShaper(func(x Fraction) Sample {
 	return Sample(math.Sin(x.Float64() * 2 * math.Pi))
